e61207508e7364d6975da5c28314d78c4db9b577
ndezelak <nejc.dezelak@gmail.com>
2017-01-15 11:09:51 +0100

    + Improved code documentation

--- Functions/processing.py
+++ Functions/processing.py
@@ -1,7 +1,9 @@
 # Module external functions:
 #                           - fill_tables(students_sorted,sorted_companies,matrix,algorithm_step)
+#                           - fill_left_places(passed_students, finished_students,sorted_companies, system)
+#                           - post_process(students,sorted_companies,system)
 # Created: 29/11/2016
-# Last Change: 12/01/2017
+# Last Change: 15/01/2017
 
 from config import *
 
@@ -25,10 +27,7 @@ def fill_tables(passed_students, finished_students,sorted_companies, system, alg
 
 
 def fill_student(algorithm_step, student, system, sorted_companies):
-    # Data is buffered as it may happen that the algorithm has no solution
-    #student_companies = student.companies
     student_seats = student.seats
-
     num_companies = len(sorted_companies)
 
     for i in range(0,len(sorted_companies)):
@@ -41,14 +40,11 @@ def fill_student(algorithm_step, student, system, sorted_companies):
                         if student_seats[row]!=0:
                             row = row +1
                             continue
-                        #if len(sorted_companies[num_companies -1 -i].seats[row]) < AVAILABLE_SEATS:
+                        # If there is enough space at the companies table then add student to the round
                         if len(sorted_companies[i].seats[row]) < AVAILABLE_SEATS:
-                            #sorted_companies[num_companies -1 -i].seats[row].append(student)
-                            #student_seats[row]=sorted_companies[num_companies -1 -i]
                             sorted_companies[i].seats[row].append(student)
                             student_seats[row]=sorted_companies[i]
                             break
-
                         row = row +1
                # If you could not find a seat at the company
                 if row >= 4:
@@ -92,7 +88,7 @@ def reorder_seats(student, conflicted_company):
             for seat_free in free_seats:
                 # there is a solution!
                 if len(student.seats[seat_filled].seats[seat_free]) < AVAILABLE_SEATS:
-                        print("Solution found!")
+                        print("Solution FOUND!")
                         # Delete student from the companies round
                         student.seats[seat_filled].seats[seat_filled].remove(student)
                         # Add student to the other companies round
@@ -104,8 +100,8 @@ def reorder_seats(student, conflicted_company):
                         student.seats[seat_filled] = conflicted_company
                         student.seats[seat_free] = buffer
                         return
-                        #student_seats[seat_filled].seats[seat_filled].remove()
 
+        # Student failed to find a place at the particular company
         print("Solution not found :(")
 
 
@@ -113,23 +109,29 @@ def fill_left_places(passed_students, finished_students,sorted_companies, system
     students_to_delete = []
     for student in passed_students:
         for row in range(0,4):
+            # If students seat is still empty
             if student.seats[row] == 0:
-                # find the company with the least seats at that round
-                min_seats = AVAILABLE_SEATS
                 min_company_index = -1
+                min_seats = AVAILABLE_SEATS
                 index = 0
+                # Find the company with the least occupied seats at that round
                 for company in sorted_companies:
-                    if len(company.seats[row]) < min_seats and system[student.list_id][company.list_id] == 0:
+                    # As companies are sorted companies with the list number of points are prioritized
+                    # Making sure a student is not added to a company twice
+                    if len(company.seats[row]) <= min_seats and system[student.list_id][company.list_id] == 0:
                         min_seats = len(company.seats[row])
                         min_company_index = index
                     index = index +1
 
+                # TODO: How to handle this case??
                 if min_company_index == -1:
                     print("student" + str(student.list_id) + " could not found any left seat in the row " + str(row)+"!")
                 else:
                     print("student"+str(student.list_id)+" has found a left seat in the round " + str(row) +"!")
                     sorted_companies[min_company_index].seats[row].append(student)
                     student.seats[row] = sorted_companies[min_company_index]
+                    print(" The company " + sorted_companies[min_company_index].name + " has now in total " + str( \
+                        len(sorted_companies[min_company_index].seats[row])) + " seats in that round")
         if 0 not in student.seats:
             finished_students.append(student)
             students_to_delete.append(student)
@@ -137,10 +139,8 @@ def fill_left_places(passed_students, finished_students,sorted_companies, system
     for student in students_to_delete:
         passed_students.remove(student)
 
-# Postprocessing started
 
-# TODO: Finish the postprocessing feature
-# IDEA: try to move students from a crowded round to a less crowded round. Pick the most optimal student from the crowded round.
+
 def post_process(students,sorted_companies,system):
     problematic_companies = []
     index = 0
@@ -154,40 +154,44 @@ def post_process(students,sorted_companies,system):
     print("Problematic places : ")
     print(problematic_companies)
     SOLUTION_FOUND = True
+
     # Try to solve each problematic round of a company
     for target in problematic_companies:
         sorted_rounds = []
         if SOLUTION_FOUND == False:
             print(" Could NOT find a complete solution!")
         SOLUTION_FOUND = False
+
+        # Sort filled companies round by length
         for round in range(0,4):
-            # Add length of a round to the list to be sorted afterward
             length = len(sorted_companies[target[0]].seats[round])
             if  length >= (MIN_NUM_SEATS+1):
                     sorted_rounds.append( [len(sorted_companies[target[0]].seats[round]),round]  )
-        # Sort the list afterwards
         sorted_rounds.sort(reverse=True)
 
         # Go through each enough full round of the company
         for [length,round] in sorted_rounds:
             # Pick a random student from that round
             for student in sorted_companies[target[0]].seats[round]:
-                # Check if the students company at the full round has some space left at the empty round
+                # Check if the students company at the empty round has enough space at the current round and that it wont go below
+                # the minimum number of seats if it makes an exchange
                 if len(student.seats[target[1]].seats[round]) <= AVAILABLE_SEATS and len(student.seats[target[1]].seats[target[1]]) > MIN_NUM_SEATS:
-                    print(" A solution could be found!")
-                    # Exhange the students seat
+                    print(" One solution could be found!")
+                    # company_A is the conflicting company, company_B is the "saver"
                     company_A = student.seats[round]
                     company_B = student.seats[target[1]]
 
+                    # Process company_B
                     student.seats[round] = company_B
                     company_B.seats[round].append(student)
                     company_B.seats[target[1]].remove(student)
 
+                    # Process company_A
                     student.seats[target[1]] = company_A
                     company_A.seats[round].remove(student)
                     company_A.seats[target[1]].append(student)
 
-                    # Check if you still need to reorder more
+                    # Have you solved the problem?
                     if len(company_A.seats[target[1]]) >=MIN_NUM_SEATS:
                         SOLUTION_FOUND = True
                         break
@@ -196,10 +200,10 @@ def post_process(students,sorted_companies,system):
 
             # You have found the solution so you can exit the outer for loop
             if SOLUTION_FOUND:
-                print("Solution for " + str(target))
+                print("A complete solution for " + str(target) + " has been found!")
                 break
             else:
-                print("Trying with the next round")
+                print("Trying with the next round ...")
 
 
 
--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-# Here the actual algorithm is written
+# Main fle
 from Classes.Person import *
 from test_data import *
 from Functions.rating_procedure import *
 from Functions.elimination import *
 from Functions.processing import *
 from config import *
-print('Hello world!')
+
+print('Starting the program ...')
 #students = Student(0,name="Nejc Dezelak",field_of_study=Field_of_Study.EE)
 #companies = Company(0,name="Infenion",field_of_study=[Field_of_Study.EE,Field_of_Study.MB],seats=[students])
 
@@ -13,32 +14,40 @@ students=[]
 companies=[]
 finished_students = []
 
+# Read data and create data structures
+print('Generating test data ...')
 generate_test_data(students,companies)
+
+# Create system matrix, rate students and companies and sort them
+print('Constructing the system matrix ...')
 system = generate_system_matrix(students,companies)
+print('Rating and sorting process ...')
 [sorted_companies,sorted_students]=rate_and_sort(students,companies,system)
+
+# Eliminate students with not enough points
+print('Elimination process started ...')
 [passed_students,boundary_region]=divide_students(sorted_students,MAX_STUDENT_NUMBER)
 
-
+# Occupy seats starting with perfect matches
+print('--------------  OCCUPYING SEATS ---------------')
 print("First pass, 3 points")
 fill_tables(passed_students,finished_students,sorted_companies,system,3)
 print("First pass, 2 points")
 fill_tables(passed_students,finished_students,sorted_companies,system,2)
 print("First pass, 1 point")
 fill_tables(passed_students,finished_students,sorted_companies,system,1)
-
-print (" Let us fill all the left seats")
+print ("First pass, 0 points")
 fill_left_places(passed_students,finished_students,sorted_companies,system)
 
+# Post processing of the occupied seats in order to assure minimal number of students at each table and at each round
 print(" Post processing ...")
 post_process(finished_students,sorted_companies,system)
 
 
-# TODO: Implement the boundary region elimination + what to do when students seats are still left after the complete first pass?
-print("---- RESULTS ----")
+print("---------------- RESULTS -------------------------")
 for company in sorted_companies:
     print(company.name + ":")
     for row in range(0,4):
         print(len(company.seats[row]))
 print ("Number of passed students:" + str(len(finished_students) + len(passed_students)))
 print ("Number of finished students:" + str(len(finished_students)) )
-print ('Done!')
